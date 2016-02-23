4e50375f1bb283501173a3bfdec0937547d4751d
Stamen Vasilev <stamen4o@yahoo.com>
2016-02-23 19:47:08 +0200

    Added files via upload

---
+++ FiscalLinux/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace FiscalLinux
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(401, 154);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(393, 128);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Дневен \'Z\' отчет";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Потвърдете дневен финансов отчет с нулиране";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ДА";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(393, 128);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Дневен \'X\' отчет";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "ДА";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(11, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Потвърдете дневен финансов отчет без нулиране";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.numericUpDown4);
+            this.tabPage3.Controls.Add(this.numericUpDown5);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.numericUpDown6);
+            this.tabPage3.Controls.Add(this.numericUpDown3);
+            this.tabPage3.Controls.Add(this.numericUpDown2);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.numericUpDown1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(393, 128);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Периодичен отчет";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(284, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "ДА";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(196, 64);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown4.TabIndex = 26;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(138, 64);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown5.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(27, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 17);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "До:";
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(82, 64);
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown6.TabIndex = 23;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(196, 32);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown3.TabIndex = 22;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(138, 32);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown2.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(28, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 17);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "От:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(82, 31);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown1.TabIndex = 19;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.listBox2);
+            this.tabPage4.Controls.Add(this.listBox1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(393, 128);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Архив";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(201, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(189, 121);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 121);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 180);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(420, 218);
+            this.MinimumSize = new System.Drawing.Size(420, 218);
+            this.Name = "Form1";
+            this.Text = "Fiscal Linux";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ FiscalLinux/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using FP3530;
+using System.IO;
+
+
+namespace FiscalLinux
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public FP3530.CS_BGR_FP2000_KL fp = new CS_BGR_FP2000_KLClass();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.MaximizeBox = false;
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Filter = "*.txt";
+            watcher.Created += new FileSystemEventHandler(watcher_FileCreated);
+            watcher.Path = @"C:/FPLinux/";
+            watcher.EnableRaisingEvents = true;
+            this.MaximizeBox = false;
+
+            string path = @"C:/FPLinux/Sale.txt";
+            if (File.Exists(path))
+            {
+                System.Threading.Thread.Sleep(1000);
+                MakeSell();
+            }
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = 31;
+            numericUpDown2.Minimum = 1;
+            numericUpDown2.Maximum = 12;
+            numericUpDown3.Minimum = 00;
+            numericUpDown3.Maximum = 24;
+            numericUpDown3.Value = 14;
+            numericUpDown4.Minimum = 00;
+            numericUpDown4.Maximum = 24;
+            numericUpDown4.Value = 14;
+            numericUpDown5.Minimum = 1;
+            numericUpDown5.Maximum = 12;
+            numericUpDown6.Minimum = 1;
+            numericUpDown6.Maximum = 31;
+            label2.Text = "OK";
+            
+        }
+
+         public void watcher_FileCreated(object sender, FileSystemEventArgs e)
+        {
+            if (File.Exists(@"C:/FPLinux/Sale.txt"))
+            {
+                System.Threading.Thread.Sleep(1000);
+                FP3530.CS_BGR_FP2000_KL fp = new CS_BGR_FP2000_KLClass();
+                MakeSell();
+            }
+        }
+        private void MakeSell()
+        {
+            string item = "";
+            string itemPrice = "";
+            string itemQuantity = "";
+            string itemDiscount = "";
+            string clientPayIn = "";
+            string myPath = @"C:/FPLinux/Sale.txt";
+            string[] lines = File.ReadAllLines(myPath);
+            var lineCount = lines.Length;
+            for (int i = 0; i <= lines.Length; i++)
+            {
+                try
+                {
+                    switch (lines[i])
+                    {
+                        case "Open":
+                            fp.INIT_EX2("192.168.123.123", "9100");
+                            break;
+                    }
+                    switch (lines[i])
+                    {
+                        case "Operator":
+                            fp.CMD_48_0_0("1", "0000", "1", "", "");
+                            break;
+                    }
+                    switch (lines[i])
+                    {
+                        case "OneItemSale":
+                            item = lines[i + 1];
+                            itemPrice = lines[i + 2];
+                            itemQuantity = lines[i + 3];
+                            itemDiscount = lines[i + 4];
+                            OneItemSale(item, itemPrice, itemQuantity, itemDiscount);
+                            break;
+                    }
+                    switch (lines[i])
+                    {
+                        case "Total":
+                            clientPayIn = lines[i + 1];
+                            fp.CMD_53_0_0("", "", "P", clientPayIn, "1", "");
+                            break;
+                    }
+                    switch (lines[i])
+                    {
+                        case "Close":
+                            fp.CMD_56_0_0("", "");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+
+                    break;
+                }
+                
+
+
+            }
+            string dateItems = DateTime.Now.ToString("dd-MM-yyyy----HH-mm");
+            string content = File.ReadAllText(@"C:/FPLinux/Sale.txt");
+            string path = @"C:/FPLinux/Archive/" + dateItems + ".txt";
+            TextWriter t = new StreamWriter(path, true);
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+            }
+            t.Write(content);
+            t.Close();
+            File.Delete(@"C:/FPLinux/Sale.txt");
+        }
+        private void OneItemSale(string item, string itemPrice, string itemQuantity, string itemDiscount)
+        {
+            fp.CMD_49_0_0(item, "", "Б", itemPrice, itemQuantity, itemDiscount);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Дневен финансов отчет с нулиране...";
+            string Closure = "";
+            string fmTotal = "";
+            string TotA = "";
+            string TotB = "";
+            string TotC = "";
+            string TotD = "";
+            string TotE = "";
+            string TotF = "";
+            string TotG = "";
+            string TotH = "";
+            fp.INIT_EX2("192.168.123.123", "9100");
+            fp.CMD_69_0_0("0", "", ref Closure, ref fmTotal, ref TotA, ref TotB, ref TotC, ref TotD, ref TotE, ref TotF, ref TotG, ref TotH);
+            label2.Text = "ОК";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Дневен финансов отчет без нулиране...";
+            string Closure = "";
+            string fmTotal = "";
+            string TotA = "";
+            string TotB = "";
+            string TotC = "";
+            string TotD = "";
+            string TotE = "";
+            string TotF = "";
+            string TotG = "";
+            string TotH = "";
+            fp.INIT_EX2("192.168.123.123", "9100");
+            fp.CMD_69_0_0("2", "", ref Closure, ref fmTotal, ref TotA, ref TotB, ref TotC, ref TotD, ref TotE, ref TotF, ref TotG, ref TotH);
+            label2.Text = "ОК";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Отчет от дата до дата...";
+            string startDate = numericUpDown1.Value.ToString().PadLeft(2, '0') + numericUpDown2.Value.ToString().PadLeft(2, '0') + numericUpDown3.Value.ToString().PadLeft(2, '0');
+            string endDate = numericUpDown6.Value.ToString().PadLeft(2, '0') + numericUpDown5.Value.ToString().PadLeft(2, '0') + numericUpDown4.Value.ToString().PadLeft(2, '0');
+            fp.INIT_EX2("192.168.123.123", "9100");
+            fp.CMD_79_0_0(startDate, endDate);
+            label2.Text = "ОК";
+        }
+
+     }
+}
